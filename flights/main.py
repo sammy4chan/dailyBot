a5002799188aa660c0c07a4ffaac37bb22fad5e1
sammy4chan <khohsam@gmail.com>
2023-04-17 19:46:19 +0800

    added bs4 parsing

--- flights/main.py
+++ flights/main.py
@@ -2,6 +2,7 @@
 
 
 import time
+from bs4 import BeautifulSoup
 from getSource import getSource
 
 #scrape 10 a day
@@ -9,4 +10,7 @@ dayInterval = (10, 24)
 for i in range(dayInterval[0], dayInterval[1]+1):
     link = f"https://www.airasia.com/flights/search/?origin=KUL&destination=PEN&departDate={i}/06/2023&tripType=O&adult=1&child=0&infant=0&locale=en-gb&currency=MYR&ule=true&cabinClass=economy&uce=false&ancillaryAbTest=false&isOC=false&isDC=false&promoCode=&type=paired&airlineProfile=all&upsellWidget=true&upsellPremiumFlatbedWidget=true"
     
-    getSource(link)
+    source = getSource(link)
+    
+    #time to parse the link
+    soup = BeautifulSoup(source, "html.parser")
