0048b2b33dc488f283bdc924c2632e45d7bc9046
sammy4chan <khohsam@gmail.com>
2023-04-18 08:59:34 +0800

    added scroll to get more than 6 flight infos

--- flights/getSource.py
+++ flights/getSource.py
@@ -3,11 +3,24 @@
 #returns html source object
 
 from selenium import webdriver
+from time import sleep
 
 def getSource(link):
     driver = webdriver.Chrome()
+    driver.implicitly_wait(1)
     
     driver.get(link)
+    #driver.execute_script("window.scrollTo(0, document.getElementById('journey-container').scrollHeight)")
+    #scroll agent
+    for i in range(2):
+        driver.execute_script("window.scrollTo(0, document.body.scrollHeight)")
+        
+        sleep(0.5)
+    
     source = driver.page_source
     driver.quit()
     return source
+
+#test code
+from bs4 import BeautifulSoup
+print(BeautifulSoup(getSource("https://www.airasia.com/flights/search/?origin=KUL&destination=PEN&departDate=10/06/2023&tripType=O&adult=1&child=0&infant=0&locale=en-gb&currency=MYR&ule=true&cabinClass=economy&uce=false&ancillaryAbTest=false&isOC=false&isDC=false&promoCode=&type=paired&airlineProfile=all&upsellWidget=true&upsellPremiumFlatbedWidget=true"), "html.parser").prettify())
