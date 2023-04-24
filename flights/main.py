50cdc8b20059d1d664686b9a74247eee64c00937
sammy4chan <khohsam@gmail.com>
2023-04-25 00:32:47 +0800

    outputs to db

--- flights/main.py
+++ flights/main.py
@@ -1,29 +1,35 @@
 from bs4 import BeautifulSoup
+from datetime import datetime
 from getSource import getSource
+#from sqlite3 import Error
+from db_folder.inserting_data import create_connection, create_date, create_entry
 
 #scrape 10 a day
 def flights():
     allFlightInfo = [] #formatted like [date, slot1, slot2, ...slot10]
     dayInterval = (10, 24)
     for i in range(dayInterval[0], dayInterval[1]+1):
-        return_arr = [f"{i}/6/2023"]
-        
         link = f"https://www.airasia.com/flights/search/?origin=KUL&destination=PEN&departDate={i}/06/2023&tripType=O&adult=1&child=0&infant=0&locale=en-gb&currency=MYR&ule=true&cabinClass=economy&uce=false&ancillaryAbTest=false&isOC=false&isDC=false&promoCode=&type=paired&airlineProfile=all&upsellWidget=true&upsellPremiumFlatbedWidget=true"
         
-        #time to parse the link
-        #soup = BeautifulSoup(open("./flights/sample.html", "r"), "html.parser") #offline ver
         soup = BeautifulSoup(getSource(link), "html.parser")
-        
         journey_container = soup.find(id="journey-container")
-        times = [i.text.strip() for i in journey_container.find_all("p", class_="cipHcF")] #cipHcF for time text <p>
-        prices = [i.text.strip() for i in journey_container.find_all("p", class_="djKEhy")] #djKEhy for price text <p>
         
-        #take the first x values and returns them as a string to be sent by bot
-        for i in range(10):
-            return_arr.append(f"{times[(2*i)]} --> {times[(2*i)+1]} : RM{prices[i]}")
-        allFlightInfo.append(return_arr)
-
-    return allFlightInfo
+        conn = create_connection("./db_folder/flights.db")
+        with conn:
+            dates = (datetime.now().strftime("%d/%m/%Y"), f"{i}/06/2023") #flight dates
+            create_date(conn, dates)
+            
+            #time to parse the link
+            #soup = BeautifulSoup(open("./flights/sample.html", "r"), "html.parser") #offline ver
+            
+            times = [i.text.strip() for i in journey_container.find_all("p", class_="cipHcF")] #cipHcF for time text <p>
+            prices = [i.text.strip() for i in journey_container.find_all("p", class_="djKEhy")] #djKEhy for price text <p>
+            
+            #take the first x values and returns them as a string to be sent by bot
+            for j in range(10):
+                entry = (f"{i}/06/2023", f"{times[(2*j)]}", f"{times[(2*j)+1]}", f"RM{prices[j]}")
+                create_entry(conn, entry)
+        
 
 if __name__ == "__main__":
     try:
