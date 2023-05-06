d25e64e914c2cde80ae33d795427e799c2e198f4
sammy4chan <khohsam@gmail.com>
2023-05-06 23:15:42 +0800

    error handling 1

--- flights/main.py
+++ flights/main.py
@@ -2,35 +2,48 @@ from bs4 import BeautifulSoup
 from datetime import datetime
 from getSource import getSource
 #from sqlite3 import Error
-from db_folder.inserting_data import create_connection, create_date, create_entry
+from db_folder.inserting_data import create_connection, create_entry
+from string import Template
+from datetime import date, timedelta
 
 #scrape 10 a day
-def flights():
-    queryTime = datetime.now().strftime("%d/%m/%Y %H:%M:%S")
-    dayInterval = (10, 24)
-    for i in range(dayInterval[0], dayInterval[1]+1):
-        link = f"https://www.airasia.com/flights/search/?origin=KUL&destination=PEN&departDate={i}/06/2023&tripType=O&adult=1&child=0&infant=0&locale=en-gb&currency=MYR&ule=true&cabinClass=economy&uce=false&ancillaryAbTest=false&isOC=false&isDC=false&promoCode=&type=paired&airlineProfile=all&upsellWidget=true&upsellPremiumFlatbedWidget=true"
-        
-        soup = BeautifulSoup(getSource(link), "html.parser")
-        journey_container = soup.find(id="journey-container")
-        
-        conn = create_connection("./db_folder/flights.db")
-        with conn:
-            #time to parse the link
-            #soup = BeautifulSoup(open("./flights/sample.html", "r"), "html.parser") #offline ver
-            
-            times = [i.text.strip() for i in journey_container.find_all("p", class_="cipHcF")] #cipHcF for time text <p>
-            prices = [i.text.strip() for i in journey_container.find_all("p", class_="djKEhy")] #djKEhy for price text <p>
-            
-            #take the first x values and returns them as a string to be sent by bot
-            for j in range(10):
-                entry = (queryTime, f"{i}/06/2023", f"{times[(2*j)]}", f"{times[(2*j)+1]}", prices[j])
-                create_entry(conn, entry)
+def getFlight(link:str, flightDate:str, queryTime):
+    soup = BeautifulSoup(getSource(link), "html.parser")
+    journey_container = soup.find(id="journey-container")
     
+    conn = create_connection("./db_folder/flights.db")
+    with conn:
+        #time to parse the link
+        #soup = BeautifulSoup(open("./flights/sample.html", "r"), "html.parser") #offline ver
+        
+        times = [i.text.strip() for i in journey_container.find_all("p", class_="cipHcF")] #cipHcF for time text <p>
+        prices = [i.text.strip() for i in journey_container.find_all("p", class_="djKEhy")] #djKEhy for price text <p>
+        
+        #take the first x values and returns them as a string to be sent by bot
+        for j in range(10):
+            entry = (queryTime, str(flightDate), f"{times[(2*j)]}", f"{times[(2*j)+1]}", prices[j])
+            create_entry(conn, entry)
+
     return "sucess"
 
+#try for 3 attempts: return a "[error] on querytime: __ flightDate: __"
+
 if __name__ == "__main__":
-    try:
-        print(flights())
-    except IndexError as e:
-        print(f"Index error {e}")
+    link = Template("https://www.airasia.com/flights/search/?origin=KUL&destination=PEN&departDate=$date&tripType=O&adult=1&child=0&infant=0&locale=en-gb&currency=MYR&ule=true&cabinClass=economy&uce=false&ancillaryAbTest=false&isOC=false&isDC=false&promoCode=&type=paired&airlineProfile=all&upsellWidget=true&upsellPremiumFlatbedWidget=true")
+    
+    for single_date in (date(2023, 6, 10) + timedelta(n) for n in range(15)):
+        flightDate = single_date.strftime("%d/%m/%y")
+        flightLink = link.substitute(date=(flightDate))
+        queryTime = datetime.now().strftime("%d/%m/%Y %H:%M:%S")
+        
+        for attempts in range(3):
+            try:
+                getFlight(link=flightLink, flightDate=flightDate, queryTime=queryTime)
+                break
+            except IndexError as ie:
+                print(ie)
+                if attempts == 2:
+                    print(f"Index Error unable to handle at {queryTime} flightDate -> {flightDate}")
+            except Exception as err:
+                #general error handling
+                print(f"Unexpected {err=}, {type(err)=}")
