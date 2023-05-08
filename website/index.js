45817aec03dbf3e8a7148e2b9dcb031276446ec4
sammy4chan <khohsam@gmail.com>
2023-05-08 19:12:41 +0800

    minor cleanup

--- website/index.js
+++ website/index.js
@@ -1,5 +1,6 @@
 import getData from "./get_data.js";
 
 getData("13/06/2023").then((rows) => {
-  console.log(rows);
+  //do smth with rows: we are going to parse it in another function: might make a new module of that
+  
 });
