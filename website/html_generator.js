a252d48a38fdac11e8b7f6530e4bbbf9943e6358
sammy4chan <khohsam@gmail.com>
2023-05-10 09:53:37 +0800

    added html generator tasks

---
+++ website/html_generator.js
@@ -0,0 +1,4 @@
+//1. display html code according to corresponding date button pressed
+//the only way to maintain scalable code for different days
+//-- add code to render number of buttons depending on length of flightdate queries
+
