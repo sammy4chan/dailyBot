14729eb550a6e86fb2b5f487fc09086a5f1f2714
sammy4chan <khohsam@gmail.com>
2023-05-14 02:13:13 +0800

    finished simple getSource module

--- ktmb/getSource.py
+++ ktmb/getSource.py
@@ -4,6 +4,8 @@
 
 from selenium import webdriver
 from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import Select
+from bs4 import BeautifulSoup
 from time import sleep
 
 #link is fixed navigation isnt
@@ -11,23 +13,35 @@ from time import sleep
 #calculate relative month scroll distance
 def getSource():
     driver = webdriver.Chrome()
+    driver.get("https://online.ktmb.com.my/")
     driver.implicitly_wait(50)
     
-    driver.get("https://online.ktmb.com.my/")
-    driver.find_element(By.ID, "journey-container")
-    sleep(1)
-    #driver.execute_script("window.scrollTo(0, document.getElementById('journey-container').scrollHeight)")
-    #scroll agent
-    for i in range(2):
-        driver.execute_script("window.scrollTo(0, document.body.scrollHeight)")
-        
-        sleep(0.5)
+    #close ad
+    driver.find_element(By.XPATH, "/html/body/div[2]/div/div/div/div[2]/div/div[2]/button").click()
     
-    source = driver.page_source
+    #select location
+    Select(driver.find_element(By.XPATH, "/html/body/div[3]/div/form/div/div[1]/select")).select_by_value("19100") #kl sentral
+    Select(driver.find_element(By.XPATH, "/html/body/div[3]/div/form/div/div[3]/select")).select_by_value("600") #pen
+    
+    #select date
+    driver.find_element(By.XPATH, "/html/body/div[3]/div/form/div/div[5]/div/div[1]/input").click() #click on depDate
+    
+    driver.find_element(By.XPATH, "/html/body/section/div/div[1]/section/header/div[2]/button[2]").click() # next month btn
+    
+    calendar = driver.find_element(By.XPATH, "/html/body/section/div/div[1]/section/div[2]")
+    calendar.find_element(By.XPATH, ".//*[text()='30' and not(contains(@class, 'is-previous-month') or contains(@class, 'is-next-month'))]").click()
+    
+    driver.find_element(By.XPATH, "/html/body/section/div/a[2]").click() #click on oneway btn
+    
+    driver.find_element(By.XPATH, "/html/body/div[3]/div/form/div/div[5]/div/div[4]/button").click() #search/submit btn
+    
+    sleep(2)
+    srcHtml = driver.find_element(By.XPATH, "/html/body/div[2]/div[1]/div/div[2]/div[1]/div[3]/div/table/tbody").get_attribute("outerHTML")
     driver.quit()
-    return source
+    
+    return srcHtml #return after closing browser instance
 
 #test code
 if __name__ == '__main__':
-    from bs4 import BeautifulSoup
-    print(BeautifulSoup(getSource("https://www.airasia.com/flights/search/?origin=KUL&destination=PEN&departDate=10/06/2023&tripType=O&adult=1&child=0&infant=0&locale=en-gb&currency=MYR&ule=true&cabinClass=economy&uce=false&ancillaryAbTest=false&isOC=false&isDC=false&promoCode=&type=paired&airlineProfile=all&upsellWidget=true&upsellPremiumFlatbedWidget=true"), "html.parser").prettify())
+    soup = BeautifulSoup(getSource(), "html.parser")
+    print(soup.prettify())
