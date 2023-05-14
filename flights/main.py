e6f83f0d40dea15bf15b9eb13ac9d03a7b2474e5
sammy4chan <khohsam@gmail.com>
2023-05-14 18:19:50 +0800

    cleaned up main.py and functions

--- flights/main.py
+++ flights/main.py
@@ -1,62 +1,7 @@
-from bs4 import BeautifulSoup
-from getSource import getSource
 #from db_folder.inserting_data import create_connection, create_entry
-import sqlite3
-from sqlite3 import Error
 from string import Template
 from datetime import date, timedelta, datetime
-
-def create_connection(db_file):
-    """ create a database connection to the SQLite database
-        specified by db_file
-    :param db_file: database file
-    :return: Connection object or None
-    """
-    conn = None
-    try:
-        conn = sqlite3.connect(db_file)
-    except Error as e:
-        print(e)
-
-    return conn
-
-def create_entry(conn, task):
-    """
-    Create a new task
-    :param conn:
-    :param task:
-    :return:
-    """
-    sql = ''' INSERT INTO flights(queryTime, flightDate, depTime, arrTime, price)
-              VALUES(?,?,?,?,?) '''
-    cur = conn.cursor()
-    cur.execute(sql, task)
-    conn.commit()
-    return cur.lastrowid
-
-#scrape 10 a day
-def getFlight(link:str, flightDate:str, queryTime, db_location):
-    soup = BeautifulSoup(getSource(link), "html.parser")
-    journey_container = soup.find(id="journey-container")
-    
-    conn = create_connection(db_location)
-    with conn:
-        #time to parse the link
-        #soup = BeautifulSoup(open("./flights/sample.html", "r"), "html.parser") #offline ver
-        
-        times = [i.text.strip() for i in journey_container.find_all("p", class_="cipHcF")] #cipHcF for time text <p>
-        prices = [i.text.strip() for i in journey_container.find_all("p", class_="djKEhy")] #djKEhy for price text <p>
-        
-        #raise an early error >:((
-        if len(times) < 10 or len(prices) < 10:
-            return IndexError
-        
-        #take the first x values and returns them as a string to be sent by bot
-        for j in range(10):
-            entry = (queryTime, str(flightDate), f"{times[(2*j)]}", f"{times[(2*j)+1]}", prices[j])
-            create_entry(conn, entry)
-
-    return "sucess"
+from getSource import getFlight
 
 #try for 3 attempts: return a "[error] on querytime: __ flightDate: __"
 
